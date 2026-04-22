b27f28cdfac5f37ba6d9604fe9d3877351f67a71
atekwana <kaseyatekwana@protonmail.com>
2026-04-22 12:05:22 -0400

    created a config file for root user to be able to access nodes

--- kubernetes-local-setup/k8s-setup/kubeadm/win_linux_macintel/set-kubeconfig.sh
+++ kubernetes-local-setup/k8s-setup/kubeadm/win_linux_macintel/set-kubeconfig.sh
@@ -10,11 +10,20 @@ KUBE_CONFIG_DST=$HOME_DIR/.kube/config
 mkdir -p "$HOME_DIR/.kube"
 
 if [ -f "$KUBE_CONFIG_SRC" ]; then
+
+  # set up for root user
+  mkdir -p /.kube/config
+  cp -f "$KUBE_CONFIG_SRC" /root/.kube/config
+  chmod 644 /root/.kube/config
+  echo "KUBECONFIG INSTALLED SUCCESSFULLY FOR ROOT USER"
+
+  # set up for vagrant user
   cp -f "$KUBE_CONFIG_SRC" "$KUBE_CONFIG_DST"
   chown $USER:$USER "$HOME_DIR/.kube"
   chmod 644 "$KUBE_CONFIG_DST"
-  echo "kubeconfig installed successfully"
+  echo "KUBECONFIG INSTALLED SUCCESSFULLY FOR VAGRANT USER"
+
 else
-  echo "ERROR: $KUBE_CONFIG_SRC not found — kubeadm init may have failed"
+  echo "ERROR --- $KUBE_CONFIG_SRC NOT FOUND! — KUBEINIT FAILED!"
   exit 1
 fi
