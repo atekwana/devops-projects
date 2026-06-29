53f5d6cfb589da572dc092ef0954ffa46028ba4b
atekwana <kaseyatekwana@protonmail.com>
2026-06-29 17:21:44 -0400

    bootstrap issue

--- kubernetes-local-setup/k8s-javaApp/win_linux_macintel/bootstrap_kmaster.sh
+++ kubernetes-local-setup/k8s-javaApp/win_linux_macintel/bootstrap_kmaster.sh
@@ -4,10 +4,10 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=192.168.33.2 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>&1532024A-02FF-1666-0000-796049510000.mod
+kubeadm init --apiserver-advertise-address=192.168.33.2 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>&1
 
 echo "[TASK 3] Deploy Calico network"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml >/dev/null 2>&1
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
