3a58fecb82381bee14f84024d0f87af11ce80805
atekwana <kaseyatekwana@protonmail.com>
2026-06-30 00:26:26 -0400

    bootstrap issue

--- kubernetes-local-setup/k8s-javaApp/win_linux_macintel/bootstrap_kworker.sh
+++ kubernetes-local-setup/k8s-javaApp/win_linux_macintel/bootstrap_kworker.sh
@@ -8,4 +8,17 @@ cp /vagrant/joincluster.sh /joincluster.sh
 
 echo "[TASK 2] Execute join command"
 bash /joincluster.sh
+#!/bin/bash
+## Worker node initialization script
+## Runs after bootstrap.sh completes on kworker1 and kworker2
 
+echo "[TASK 1] Wait for kmaster API server to be ready"
+until curl -k -s --max-time 2 https://192.168.33.2:6443 >/dev/null; do
+  sleep 5
+done
+
+echo "[TASK 2] Join node to Kubernetes Cluster"
+cp /vagrant/joincluster.sh /joincluster.sh
+
+echo "[TASK 3] Execute join command"
+bash /joincluster.sh
